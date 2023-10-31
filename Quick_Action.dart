1bf8f84e9b20a8ac02fc33d44bfa1867c4772c53
Khoob Coding <84430096+khoobcoding@users.noreply.github.com>
2023-10-31 05:31:09 +0530

    Add files via upload

---
+++ Quick_Action.dart
@@ -0,0 +1,86 @@
+import 'package:example/Call_Page.dart';
+import 'package:example/Chat_Page.dart';
+import 'package:example/Search_Page.dart';
+import 'package:flutter/material.dart';
+import 'package:quick_actions/quick_actions.dart';
+
+class QuickActionApp extends StatefulWidget {
+  const QuickActionApp({super.key});
+
+  @override
+  State<QuickActionApp> createState() => _QuickActionAppState();
+}
+
+class _QuickActionAppState extends State<QuickActionApp> {
+  QuickActions quickactions=const QuickActions();
+  
+  @override
+  void initState() {
+    quickactions.initialize((type) {
+      if(type=='search'){
+        Navigator.push(context, MaterialPageRoute(builder:(context) {
+          return SearchPage();
+        },));
+      }
+      if(type=='chat'){
+        Navigator.push(context, MaterialPageRoute(builder:(context) {
+          return ChatPage();
+        },));
+      }
+      if(type=='call'){
+        Navigator.push(context, MaterialPageRoute(builder:(context) {
+          return CallPage();
+        },));
+      }
+    });
+    quickactions.setShortcutItems([
+      const ShortcutItem(type: 'search', localizedTitle: 'Search',
+      icon: 'search'),
+      const ShortcutItem(type: 'chat', localizedTitle: 'Chat',
+      icon: 'chat'),
+      const ShortcutItem(type: 'call', localizedTitle: 'Call',
+      icon: 'call')
+    ]);
+    super.initState();
+  }
+  @override
+  void dispose() {
+    quickactions.clearShortcutItems();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.indigo,
+          title: const Text(
+            'Quick Action App ',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 250,
+                color: Colors.indigo,
+                child: const Center(
+                    child: Icon(Icons.home, size: 100, color: Colors.white)),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'Home Page ',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Color.fromARGB(255, 62, 61, 61),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ));
+  }
+}
